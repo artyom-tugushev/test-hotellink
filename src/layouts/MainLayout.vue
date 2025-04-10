f9b178a9353fe635cc37a711e2ae4fbe81be8b85
artyom-tugushev <152002484+artyom-tugushev@users.noreply.github.com>
2025-04-10 16:32:01 +0300

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <router-view> </router-view>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,7 @@
+<script setup>
+//
+</script>
+
+<template>
+  <q-page class="flex flex-center"> ttt </q-page>
+</template>
